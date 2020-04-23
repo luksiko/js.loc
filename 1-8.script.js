01227a20552a8f69d51a99247335b4d3261fe424
luksiko <luksiko90@gmail.com>
2020-04-23 14:50:35 +0500

    Это мой 4й комит

---
+++ 1-8.script.js
@@ -0,0 +1,92 @@
+//'use strict';
+
+// // a = 5;
+
+// console.log(a);
+
+// var number = 5;
+// var string = "HELLO!";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
+
+// console.log(4/0);
+// console.log('string'*9);
+
+// let something;
+// console.log(something);
+
+// // let person = {
+// //   name: "John",
+// //   age: 25,
+// //   isMarried: false
+// // };
+
+// // console.log(person['name']);
+
+// // let arr = ['plum.png','or.jpg','apple.bmp'];
+
+// // console.log(arr[2])
+
+// // // alert("Hello World!")
+
+// // // let answer = confirm("Are you here?")
+
+// // let answer = +prompt("Есть ли вам 18?", "Да", "" );
+
+// //  console.log(typeof(answer));
+
+// // console.log("arr" + " - object");
+// // console.log(4 + " - object");
+
+// let incr = 10,
+//     decr = 10;
+
+// // incr++;
+// // decr--;
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(5%2);
+// console.log("2" ===  2);
+
+// let isChecked = true,
+//     isClose = true;
+
+// console.log(isChecked || isClose);
+
+// if (2*4 == 7) {
+//   console.log("Верно!")
+// } else {
+//   console.log("Неверно (");
+// }
+
+let num = 50;
+
+// if (num < 49) {
+//   console.log("Неверно")
+// } else if (num > 100) {
+//   console.log("Много!")
+// } else {
+//   console.log("Верно!")
+// };
+
+// (num == 50) ? console.log("Верно!"): console.log("Неверно!");
+
+// switch (num) {
+//   case num < 49:
+//     console.log("Неверно!");
+//     break;
+//   case num > 100:
+//     console.log("Mnogo!");
+//     break;
+//   case 50:
+//     console.log("Верно!");
+//     break;
+//     default:
+//       console.log("чтото не так!");
+// break;
+// }
