4e6f348f65f8ccb3e99f215d9aae90b57064c495
Aleksey <luksiko90@gmail.com>
2020-04-23 16:16:09 +0500

    мой первый сабж от luksiko

--- script.js
+++ script.js
@@ -1,24 +1,59 @@
 // let money = "Ваш бюджет на месяц?";
 
-var d = new Date();
-var day = d.getDate();
-var month = d.getMonth() + 1;
-var year = d.getFullYear();
+// var d = new Date();
+// var day = d.getDate();
+// var month = d.getMonth() + 1;
+// var year = d.getFullYear();
 
-let money = prompt("Ваш бюджет на месяц в $?", "300", "");
-let time = prompt("Введите дату в формате YYYY-MM-DD", year + "-" + month + "-" + day, "");
+// let money = prompt("Ваш бюджет на месяц в $?", "300", "");
+// let time = prompt("Введите дату в формате YYYY-MM-DD", year + "-" + month + "-" + day, "");
 
-let article = prompt('Введите обязательную статью расходов в этом месяце', "Статья", "");
-let cost = prompt('Во сколько обойдется?', "300$", "");
+// let article = prompt('Введите обязательную статью расходов в этом месяце', "Статья", "");
+// let cost = prompt('Во сколько обойдется?', "300$", "");
 
-let appData = {
-  bujet: money,
-  date: time,
-  expenses: {
-    article : cost
-    },
-  optionalExpenses: null,
-  income: null,
-  savings: false,
-};
-alert('Ваш бюджет на сегодня: '+money/30+'$');
+// let appData = {
+//   bujet: money,
+//   date: time,
+//   expenses: {
+//     article : cost
+//     },
+//   optionalExpenses: null,
+//   income: null,
+//   savings: false,
+// };
+// alert('Ваш бюджет на сегодня: '+money/30+'$');
+
+let num = 50;
+
+// while (num < 55) {
+//   console.log(num);
+//   num++;
+// }
+
+// do {
+//   console.log(num);
+//   num++;
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 8; i++) {
+//   console.log(i);
+// };
+
+// for (let i = 1; i < 8; i++) {
+//   if (i == 6) {
+//    //  break
+//    continue;
+//   }
+//    console.log(i);
+//  }
+
+//  let x = 5; alert( x++ );
+// console.log([ ] + 1 + 2);
+// let y = 1; let x = y = 2; alert(x);
+
+// alert( "1"[0] );
+
+// console.log( 2 && 1 && null && 0 && undefined );
+
+console.log( !!( a && b ) = (a && b))
