3d969c0b1f0f29e1286ef2a290353ebe5faf55d5
luksiko <luksiko90@gmail.com>
2020-04-23 14:20:27 +0500

    Это мой 3й комит

--- script.js
+++ script.js
@@ -1,59 +1,71 @@
-// 'use strict';
+//'use strict';
 
-a = 5;
+// // a = 5;
 
-console.log(a);
+// console.log(a);
 
-var number = 5;
-var string = "HELLO!";
-var sym = Symbol();
-var boolean = true;
-null;
-undefined;
-var obj = {};
+// var number = 5;
+// var string = "HELLO!";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
 
-console.log(4/0);
-console.log('string'*9);
+// console.log(4/0);
+// console.log('string'*9);
 
-let something;
-console.log(something);
+// let something;
+// console.log(something);
 
-// let person = {
-//   name: "John",
-//   age: 25,
-//   isMarried: false
-// };
+// // let person = {
+// //   name: "John",
+// //   age: 25,
+// //   isMarried: false
+// // };
 
-// console.log(person['name']);
+// // console.log(person['name']);
 
-// let arr = ['plum.png','or.jpg','apple.bmp'];
+// // let arr = ['plum.png','or.jpg','apple.bmp'];
 
-// console.log(arr[2])
+// // console.log(arr[2])
 
-// // alert("Hello World!")
+// // // alert("Hello World!")
 
-// // let answer = confirm("Are you here?")
+// // // let answer = confirm("Are you here?")
 
-// let answer = +prompt("Есть ли вам 18?", "Да", "" );
+// // let answer = +prompt("Есть ли вам 18?", "Да", "" );
 
-//  console.log(typeof(answer));
+// //  console.log(typeof(answer));
 
-// console.log("arr" + " - object");
-// console.log(4 + " - object");
+// // console.log("arr" + " - object");
+// // console.log(4 + " - object");
 
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
 
-// incr++;
-// decr--;
+// // incr++;
+// // decr--;
 
-console.log(incr++);
-console.log(decr--);
+// console.log(incr++);
+// console.log(decr--);
 
-console.log(5%2);
-console.log("2" ===  2);
+// console.log(5%2);
+// console.log("2" ===  2);
 
-let isChecked = true,
-    isClose = true;
+// let isChecked = true,
+//     isClose = true;
 
-console.log(isChecked || isClose);
+// console.log(isChecked || isClose);
+
+// if (2*4 == 7) {
+//   console.log("Верно!")
+// } else {
+//   console.log("Неверно (");
+// }
+
+let num = 50;
+
+if (condition) {
+  
+}
